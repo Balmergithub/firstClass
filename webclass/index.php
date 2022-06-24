484f588e0161ff5488bfc1ac0c9e9d7acf89b536
Balmergithub <mwongeraligo@gmail.com>
2022-06-25 00:25:50 +0300

    Initial commit

---
+++ webclass/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Development II</title>
+</head>
+<body>
+    <div>
+        <p>This is a web development web app using PHP scripts</p>
+    </div>
+    <?php
+        $txt = 'I Love Programming Language';
+        $x = 10;
+        $y = 15;
+
+        echo $txt;
+        echo '</br>';
+        echo $x;
+        echo '</br>';
+        echo $y;
+        echo '</br>';
+    ?>
+</body>
+</html>
